3656233681ff3391bfa0ca1c6386bb71c102c8c4
Muhammad Rashid <rashid1428@gmail.com>
2024-10-25 21:45:58 +0500

    removed data classes, and update app name

--- app/src/main/java/com/app/vivi/domain/model/PatientMenuItem.kt
+++
@@ -1,9 +0,0 @@
-package com.app.vivi.domain.model
-
-import com.app.vivi.constants.MenuOptions
-
-data class PatientMenuItem(
-    val icon: Int,
-    val name: String,
-    val id: MenuOptions
-)
--- app/src/main/java/com/app/vivi/features/homescreen/display/model/PatientModel.kt
+++
@@ -1,9 +0,0 @@
-package com.app.vivi.features.homescreen.display.model
-
-import android.os.Parcelable
-import kotlinx.parcelize.Parcelize
-
-@Parcelize
-data class PatientModel(
-    val name: String
-): Parcelable
