2655e4b52c9038a276219c73da68c9f5aa6a998d
Muhammad Rashid <rashid1428@gmail.com>
2024-10-31 10:45:26 +0500

    Implemented login main screen, login email screen

--- app/src/main/java/com/app/vivi/extension/ExtActivity.kt
+++ app/src/main/java/com/app/vivi/extension/ExtActivity.kt
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.NavController
+import androidx.navigation.NavDirections
+import androidx.navigation.NavOptions
 import kotlinx.coroutines.launch
 
 fun AppCompatActivity.collectWhenStarted(action: suspend () -> Unit) {
@@ -12,4 +15,11 @@ fun AppCompatActivity.collectWhenStarted(action: suspend () -> Unit) {
             action()
         }
     }
+}
+
+fun NavController.navigateWithSingleTop(action: NavDirections) {
+    val navOptions = NavOptions.Builder()
+        .setLaunchSingleTop(true)
+        .build()
+    this.navigate(action, navOptions)
 }
--- app/src/main/java/com/app/vivi/features/login/LoginMainFragment.kt
+++ app/src/main/java/com/app/vivi/features/login/LoginMainFragment.kt
@@ -1,13 +1,25 @@
 package com.app.vivi.features.login
 
+import android.graphics.drawable.AnimationDrawable
 import android.os.Bundle
 import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.view.animation.TranslateAnimation
+import androidx.core.content.ContextCompat
 import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.viewModels
+import androidx.navigation.NavOptions
+import androidx.navigation.fragment.findNavController
+import com.app.vivi.R
 import com.app.vivi.basefragment.BaseFragmentVB
 import com.app.vivi.databinding.FragmentLoginBinding
 import com.app.vivi.databinding.FragmentLoginMainBinding
 import com.app.vivi.extension.collectWhenStarted
+import com.app.vivi.extension.navigateWithSingleTop
+import com.app.vivi.extension.setDrawableWithSize
+import com.app.vivi.extension.showShortToast
+import com.app.vivi.features.homescreen.home.fragments.HomeFragmentLatestDirections
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collectLatest
 import java.util.*
@@ -21,20 +33,49 @@ class LoginMainFragment : BaseFragmentVB<FragmentLoginMainBinding>(FragmentLogin
         super.onViewCreated(view, savedInstanceState)
 
         binding.apply {
-            btnLogin.setOnClickListener {
-                viewModel.onLoginClicked(
-                    tieEmail.text.toString(),
-                    tiePassword.text.toString()
-                )
+            tvTagline.text =
+                getString(R.string.buy_the_right_txt, getString(R.string.app_name))
+
+            googleButton.setDrawableWithSize(requireContext(),
+                R.drawable.ic_male_placeholder,
+                70, 70,
+                resources.getDimensionPixelSize(R.dimen.sdp_10))
+
+            facebookButton.setDrawableWithSize(requireContext(),
+                R.drawable.ic_male_placeholder,
+                70, 70,
+                resources.getDimensionPixelSize(R.dimen.sdp_10))
+
+            ivBackground.post {
+                val startY = tvTitle.y // Start position from top of tvTitle
+                val endY = 0f // End position at the top of the screen
+
+                val animation = TranslateAnimation(0f, 0f, startY, endY)
+                animation.duration = 2000 // Duration in milliseconds (e.g., 2 seconds)
+                animation.fillAfter = true // Keeps the view at the final position after animation ends
+                animation.repeatCount = Animation.INFINITE // Repeat indefinitely
+                animation.repeatMode = Animation.RESTART
+
+                ivBackground.startAnimation(animation)
             }
 
-            tieEmail.doOnTextChanged { text, start, before, count ->
+//            val slideUpAnimation = AnimationUtils.loadAnimation(requireContext(), R.anim.slide_up_animation)
+//            ivBackground.startAnimation(slideUpAnimation)
+//            val animationDrawable = ivBackground.drawable as AnimationDrawable
+//            animationDrawable.start()
+
+            emailButton.setOnClickListener {
+                val action = LoginMainFragmentDirections.actionLoginMainFragmentToLoginFragment()
+                findNavController().navigateWithSingleTop(action)
+            }
+
+            /* tieEmail.doOnTextChanged { text, start, before, count ->
                 viewModel.onEmailTextChanged(text.toString())
             }
 
             tiePassword.doOnTextChanged { text, start, before, count ->
                 viewModel.onPasswordChanged(text.toString())
-            }
+            }*/
         }
 
         addObservers()
@@ -45,13 +86,13 @@ class LoginMainFragment : BaseFragmentVB<FragmentLoginMainBinding>(FragmentLogin
     private fun addObservers() {
         collectWhenStarted {
             viewModel.emailErrorFlow.collectLatest {
-                binding.tilEmail.error = it
+//                binding.tilEmail.error = it
             }
         }
 
         collectWhenStarted {
             viewModel.passwordErrorFlow.collectLatest {
-                binding.tilPassword.error = it
+//                binding.tilPassword.error = it
             }
         }
 
