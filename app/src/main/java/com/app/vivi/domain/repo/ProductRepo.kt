8594d792d7fb5e6641d763629d2b61a1bffcf8f8
Muhammad Rashid <rashid1428@gmail.com>
2024-11-23 11:55:34 +0500

    implemented shop by bean types, countries api

--- app/src/main/java/com/app/vivi/domain/repo/ProductRepo.kt
+++ app/src/main/java/com/app/vivi/domain/repo/ProductRepo.kt
@@ -5,6 +5,8 @@ import com.app.vivi.data.remote.Resource
 import com.app.vivi.data.remote.model.response.FindYourNewFavoriteProductResponse
 import com.app.vivi.data.remote.model.response.PreferenceProductResponse
 import com.app.vivi.data.remote.model.response.UserReviewsResponse
+import com.app.vivi.data.remote.model.response.searchfragment.CoffeeBeanTypesResponse
+import com.app.vivi.data.remote.model.response.searchfragment.CountriesResponse
 import com.app.vivi.data.remote.model.response.searchfragment.ShopByTypeResponse
 
 interface ProductRepo {
@@ -18,5 +20,9 @@ interface ProductRepo {
     suspend fun getUserReviews(): Resource<UserReviewsResponse>
 
     suspend fun getShopByCoffeeTypes(): Resource<ShopByTypeResponse>
+
+    suspend fun getShopByCoffeeBeanTypes(): Resource<CoffeeBeanTypesResponse>
+
+    suspend fun getShopByCountries(): Resource<CountriesResponse>
 
 }
